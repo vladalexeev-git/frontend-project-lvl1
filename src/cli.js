81a3e9fe6ea03e513bf0e35e7dc48a88781029e6
vvv <vlad@vvv.io>
2020-09-17 00:47:06 +0500

    added readline-sync and eslint

---
+++ src/cli.js
@@ -0,0 +1,6 @@
+import readlineSync from 'readline-sync';
+
+export default () => {
+    const name = readlineSync.question('May I have your name? ');
+    return console.log(`Helllo ${name}!`);
+};
