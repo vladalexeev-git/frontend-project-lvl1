e13b76859f3bbe1a8cb49430160f69db371efbc8
vvv <vlad@vvv.io>
2020-09-18 16:45:17 +0500

    fix linter problems

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
 export default () => {
-    const name = readlineSync.question('May I have your name? ');
-    return console.log(`Helllo ${name}!`);
+  const name = readlineSync.question('May I have your name? ');
+  return console.log(`Helllo ${name}!`);
 };
